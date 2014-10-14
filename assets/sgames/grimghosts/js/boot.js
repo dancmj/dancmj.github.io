7b03f843914ba48a32cc992c54f4e86420355e13
Dan <daniel.c82@gmail.com>
2014-10-14 00:17:20 -0700

    Fixed errors while loading assets

--- assets/sgames/grimghosts/js/boot.js
+++ assets/sgames/grimghosts/js/boot.js
@@ -1,6 +1,6 @@
 var bootState = {
     preload: function(){
-        game.load.image('progressBar','/assets/sgames/grimghost/progressBar.png');
+        game.load.image('progressBar','/assets/sgames/grimghost/assets/progressBar.png');
     },
     
     create: function(){
